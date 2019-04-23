b5edc3e68d4d98f5fd490a691e8faf6b43a5bb95
Jessica Quach <jessica_quach60@hotmail.com>
2019-04-23 09:35:32 +0200

    Comments added to the User-class

--- MessageSystemSolo/src/Server/User.java
+++ MessageSystemSolo/src/Server/User.java
@@ -9,40 +9,68 @@ import javax.swing.ImageIcon;
 
 import Client.Message;
 
-//Skriv gärna vad klassen gör och vem som skrivit den
+/**
+ * This class represents a User.
+ * @author Jessica & Henrik
+ *
+ */
 public class User implements Serializable {
-	@Override
-	public String toString() {
-		return "User [name=" + name + ", picture=" + picture + "]";
-	}
-
 	private String name;
 	private ImageIcon picture = null;
 
+	/**
+	 * Constructs a User-object without a chosen picture.
+	 * @param username the name of the user
+	 */
 	public User(String username) {
 		this.name = username;
 	}
 
+	/**
+	 * Constructs a User-object with a chosen picture.
+	 * @param name the name of the user
+	 * @param pic the picture the user has chosen
+	 */
 	public User(String name, ImageIcon pic) {
 		this.name = name;
 		this.picture = pic;
 	}
 
+	/**
+	 * Sets a picture to the user.
+	 * @param picture the picture to use
+	 */
 	public void setPicture(ImageIcon picture) {
 		this.picture = picture;
 	}
 
+	/**
+	 * Returns the picture
+	 * @return the ImageIcon
+	 */
 	public ImageIcon getPicture() {
 		return picture;
 	}
 
+	/**
+	 * Sets the name of the user
+	 * @param name a new username
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Returns the username
+	 * @return the username
+	 */
 	public String getName() {
 		return name;
 	}
-
+	
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", picture=" + picture + "]";
+	}
 
 }
